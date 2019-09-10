2fffc3d363852ef77522f001efeeb6dcf52e0aea
Christopher <uber.rooster.in.chains@gmail.com>
2019-09-10 07:07:23 -0700

    another commit

--- graph_node.rb
+++ graph_node.rb
@@ -1,5 +1,34 @@
+require 'set'
 class GraphNode
-  a = GraphNode.new('a')
+  attr_accessor :val, :neighbors 
+   
+  def initialize(val)
+    self.val = val 
+    self.neighbors = []
+  end
+
+  def add_neighbor(node)
+    self.neighbors << node 
+  end
+
+  def bfs(starting_node, target_value)
+      queue = [starting_node]
+      visited = Set.new()
+
+      until queue.empty?
+        node = queue.shift
+        unless visited.include?(node)
+          return node.val if node.val == target_value 
+          visted.add(node)
+          queue += node.neighbors 
+        end
+      end
+
+      return nil
+  end
+end
+
+a = GraphNode.new('a')
   b = GraphNode.new('b')
   c = GraphNode.new('c')
   d = GraphNode.new('d')
@@ -9,9 +38,3 @@ class GraphNode
   c.neighbors = [b, d]
   e.neighbors = [a]
   f.neighbors = [e]
-
-
-  def bfs(starting_node, target_value)
-    
-  end
-end
